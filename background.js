0b3961666b0d571c67aae63833b0b1a3691bb30d
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-06-26 10:38:25 +0200

    Use optional chaining (?.) and nullish coalescing (??) operators
    Handle nullish values in a canonical JS way. This also fixes a bug if no
    body text is defined in a mailto link.

--- background.js
+++ background.js
@@ -23,13 +23,13 @@ chrome.runtime.onMessage.addListener(
             code: `
             document.querySelector("div.item.btn.write").click();
             var newEmailTabIndex = document.querySelectorAll(".mail.item.closeable").length - 1;
-            document.querySelectorAll("div.input-to > textarea")[newEmailTabIndex].value = "${url.pathname}";
-            document.querySelectorAll("div.input-cc > textarea")[newEmailTabIndex].value = "${url.searchParams.get("cc")}";
-            document.querySelectorAll("div.input-bcc > textarea")[newEmailTabIndex].value = "${url.searchParams.get("bcc")}";
-            document.querySelectorAll("div.input-subject > input")[newEmailTabIndex].value = "${url.searchParams.get("subject")}";
+            document.querySelectorAll("div.input-to > textarea")[newEmailTabIndex].value = "${url.pathname ?? ''}";
+            document.querySelectorAll("div.input-cc > textarea")[newEmailTabIndex].value = "${url.searchParams?.get("cc") ?? ''}";
+            document.querySelectorAll("div.input-bcc > textarea")[newEmailTabIndex].value = "${url.searchParams?.get("bcc") ?? ''}";
+            document.querySelectorAll("div.input-subject > input")[newEmailTabIndex].value = "${url.searchParams?.get("subject") ?? ''}";
             var newEmailTextArea = document.querySelectorAll("div.input-body > textarea")[newEmailTabIndex];
-            setTimeout(() => { newEmailTextArea.value = "${url.searchParams.get("body").replaceAll(/\r?\n/g, "\\n").replaceAll(/"/g, '\\"')}" + newEmailTextArea.value; }, 500);
-          `.replaceAll(/.*"null".*/g, '')
+            setTimeout(() => { newEmailTextArea.value = "${url.searchParams.get("body")?.replaceAll(/\r?\n/g, "\\n").replaceAll(/"/g, '\\"') ?? ''}" + newEmailTextArea.value; }, 500);
+          `
           });
         }
       }
